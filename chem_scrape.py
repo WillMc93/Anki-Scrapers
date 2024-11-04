ed3c118da7327f86a91c741ce62f2312dd6f3149
Will McElhenney <william.m@wustl.edu>
2024-11-04 16:15:20 -0600

    Removed the equation scraping as infeasible

--- chem_scrape.py
+++ chem_scrape.py
@@ -23,27 +23,6 @@ def get_terms(chapter:int) -> dict:
         definition = dl.find('dd')
         yield term.text, definition.text
 
-
-# def get_equations(chapter:int) -> dict:
-#     """Scrape OpenStax Chemistry 2e Equations"""
-
-#     url = f'{CHEMISTRY_URL}/pages/{chapter}-key-equations'
-#     response = requests.get(url)
-#     response.raise_for_status()
-
-#     soup = BeautifulSoup(response.text, 'html.parser')
-#     div = soup.find('div', class_='os-eoc os-key-equations-container')
-#     sections = div.find_all('section', class_='key-equations')
-
-#     for section in sections:
-#         table = section.find('table')
-#         rows = table.find_all('td')
-#         for row in rows:
-#             mathjax = row.find('math')
-#             # Remove the annotation-eml encoding elements
-#             mathjax.find('annotation-xml').decompose()
-#             yield mathjax
-
 #%%
 if __name__ == '__main__':
     for i in range(1, 21):
